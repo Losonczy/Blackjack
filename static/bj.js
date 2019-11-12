18ed77a0fb937b7e321afb580052ce99b1ec4779
Olivér Gál <galoliver05@gmail.com>
2019-11-12 12:30:12 +0100

    implement showing random card

--- static/bj.js
+++ static/bj.js
@@ -1,10 +1,10 @@
 function showCard(){
-    createCard(createSrc(9,'C'),'player1');
+    createCard(createSrc(getRandomCard(deck)),'player1');
 }
 
-function createSrc(value,color){
+function createSrc(card){
 
-    let src="static/JPEG/"+value+color+".jpg";
+    let src="static/JPEG/"+card+".jpg";
     return src;
 }
 
@@ -12,6 +12,7 @@ function createCard(src,who){
 
     var img=document.createElement("img");
     img.src = src;
+    img.style.display = "block";
     img.id="card";
     img.className='card';
     if (who=='player1'){
@@ -23,5 +24,63 @@ function createCard(src,who){
 }
 function checkForCards(){
 
+}
+
+var suits = ["S", "H", "D", "C"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
+createDeck();
+
+function createDeck()
+{
+	deck = new Array();
+	for (var i = 0 ; i < values.length; i++)
+	{
+	    for(var x = 0; x < suits.length; x++)
+	    {
+		var card = values[i] + suits[x];
+		deck.push(card);
+	    }
+	}
+}
+
+function getRandomNumber(start, range) {
+	let getRandom = Math.floor((Math.random() * range) + start);
+	while (getRandom > range) {
+		getRandom = Math.floor((Math.random() * range) + range);
+	}
+	return getRandom
+}
+
+
+
+/*function addValueToCards(cards)
+{
+    var cardsDict{};
+    var cardValueIndex = 0
+    var cardValue;
+    for (var i = 0 ; i < cards.length; i++) {
+        if (cards[i][cardValueIndex] == "J" || cards[i][cardValueIndex][i] == "Q" || cards[i][cardValueIndex][i] == "K") {
+            }
+                cardValue = 10;
+            }
+        else if (cards[i][cardValueIndex == "A")
+            {
+                cardValue = 11;
+            }
+        else
+            {
+                cardValue = parseInt(cards[i][cardValueIndex])
+            }
+        cardsDict.push()
+
+    }
+}*/
+
+function getRandomCard(deck)
+{
+    var randomCardIndex = getRandomNumber(0, 51);
+    var card = deck[randomCardIndex]    ;
+    return card;
 }
 

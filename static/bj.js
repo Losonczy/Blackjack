ef077fc9f9ad8e8ca402f3f0fc3ed5997f9a690b
Barnuss09 <hollbarna@gmail.com>
2019-11-14 13:38:00 +0100

    Implement saty button

--- static/bj.js
+++ static/bj.js
@@ -4,156 +4,125 @@ let playerCards = [];
 let bankCards = [];
 var playerCardValues = [];
 var bankCardValues = [];
-const deck = createDeck()
-var turn='player'
+const deck = createDeck();
+var turn = 'player';
 let intOfPlayerCards = [];
 let intOfBankCards = [];
 
-//let hit=document.getElementById('hit-btn').addEventListener('click', hit())
-//document.getElementById('stay-btn').addEventListener('click', endTurn())
-
-function getIntFromObject(cards)
-{
+function getIntFromObject(cards) {
     let intArray = [];
-    for (let i = 0; i < cards.length; i++)
-    {
+    for (let i = 0; i < cards.length; i++) {
         intArray.push(parseInt(cards[i]));
     }
     return intArray;
 
 }
 
-function storeHands(who, currentCard)
-{
-    if (who === "player")
-    {
+function storeHands(who, currentCard) {
+    if (who === "player") {
         playerCards.push(currentCard);
-    }
-    else
-    {
+    } else {
         bankCards.push(currentCard);
     }
 }
 
-function dealStartingHand()
-{
+function dealStartingHand() {
     showCard('bank');
     showCard('bank');
     showCard('player');
     showCard('player');
 
 }
+
 function hit() {
     showCard('player');
     console.log(intOfPlayerCards);
     console.log(intOfBankCards);
     console.log(typeof intOfPlayerCards[2]);
-
-    /*console.log(summCardValues(bankCardValues));*/
 }
 
 
-function showCard(who){
+function showCard(who) {
     let card = getRandomCard(createDeck(), playerCards, bankCards);
     storeHands(who, card);
     createCard(createSrc(card), who);
-    //store values
-    playerCardValues = addValuesOfCards(playerCards);
-    bankCardValues = addValuesOfCards(bankCards);
-    intOfBankCards = getIntFromObject(bankCardValues);
-    intOfPlayerCards = getIntFromObject(playerCardValues);
-    //store values
+    getValueOfCards();
 }
 
 
-function createSrc(card){
+function createSrc(card) {
 
-    let src="static/JPEG/"+card+".jpg";
+    let src = "static/JPEG/" + card + ".jpg";
     return src;
 }
 
-function createCard(src,who){
+function createCard(src, who) {
 
     let img = document.createElement("img");
     img.src = src;
-    img.id="card";
-    img.className='card';
-    if (who=='player'){
-            document.getElementById('player_hand').appendChild(img);
-    }
-    else{
-            document.getElementById('bank_hand').appendChild(img);
+    img.id = "card";
+    img.className = 'card';
+    if (who == 'player') {
+        document.getElementById('player_hand').appendChild(img);
+    } else {
+        document.getElementById('bank_hand').appendChild(img);
     }
 }
 
 
-function createDeck()
-{
+function createDeck() {
     var deck = new Array();
-	for (var i = 0 ; i < values.length; i++)
-	{
-	    for(var x = 0; x < suits.length; x++)
-	    {
-		var card = values[i] + suits[x];
-		deck.push(card);
-	    }
-	}
-	return deck;
+    for (var i = 0; i < values.length; i++) {
+        for (var x = 0; x < suits.length; x++) {
+            var card = values[i] + suits[x];
+            deck.push(card);
+        }
+    }
+    return deck;
 }
 
 function getRandomNumber(start, range) {
-	let getRandom = Math.floor((Math.random() * range) + start);
-	while (getRandom > range) {
-		getRandom = Math.floor((Math.random() * range) + range);
-	}
-	return getRandom
+    let getRandom = Math.floor((Math.random() * range) + start);
+    while (getRandom > range) {
+        getRandom = Math.floor((Math.random() * range) + range);
+    }
+    return getRandom
 }
 
 
-function addValuesOfCards(cards)
-{
+function addValuesOfCards(cards) {
     let valueOfCards = [];
-    let cardsWithValues=addingValues(cards);
-    for (let i =0; i < cardsWithValues.length; i++)
-    {
+    let cardsWithValues = addingValues(cards);
+    for (let i = 0; i < cardsWithValues.length; i++) {
         valueOfCards.push(cardsWithValues[i]);
     }
     return valueOfCards;
 }
 
 
-function addingValues(cards)
-{
+function addingValues(cards) {
     let cardValues = [];
     let cardValueIndex = 0;
     let cardValue;
-    for (var i = 0 ; i < cards.length; i++) {
+    for (var i = 0; i < cards.length; i++) {
         if (cards[i][cardValueIndex] === "J" || cards[i][cardValueIndex] === "Q" || cards[i][cardValueIndex] === "K") {
-                cardValue = 10;
-            }
-        else if (cards[i][cardValueIndex] === "A")
-            {
-                cardValue = 11;
-            }
-        else if (cards[i][1] === "0")
-        {
             cardValue = 10;
+        } else if (cards[i][cardValueIndex] === "A") {
+            cardValue = 11;
+        } else if (cards[i][1] === "0") {
+            cardValue = 10;
+        } else {
+            cardValue = parseInt(cards[i][cardValueIndex])
         }
-        else
-            {
-                cardValue = parseInt(cards[i][cardValueIndex])
-            }
         cardValues.push(cardValue);
     }
     return cardValues
 }
 
-function getRandomCard(deck, playerCards, bankCards)
-{
+function getRandomCard(deck, playerCards, bankCards) {
     let randomCardIndex = getRandomNumber(0, 51);
-    let card = deck[randomCardIndex]    ;
-    while (playerCards.includes(card) || bankCards.includes(card))
-    {
+    let card = deck[randomCardIndex];
+    while (playerCards.includes(card) || bankCards.includes(card)) {
         randomCardIndex = getRandomNumber(0, 51)
         card = deck[randomCardIndex]
     }
@@ -161,47 +130,49 @@ function getRandomCard(deck, playerCards, bankCards)
     return card;
 }
 
-function endTurn(){
-    let new_button=document.getElementById('hit-btn');
-    new_button.onclick="";
+function endTurn() {
+    let new_button = document.getElementById('hit-btn');
+    new_button.onclick = "";
+    new_button = document.getElementById('stay-btn');
+    new_button.onclick = "";
+    let a = true;
+    while (a === true) {
+        showCard('bank');
+        a = false;
+    }
+    startTurn();
+
 }
 
 function startTurn() {
-    document.querySelector('.player_hand').innerHTML="";
-    document.querySelector('.bank_hand').innerHTML="";
+    
+    document.querySelector('.player_hand').innerHTML = "";
+    document.querySelector('.bank_hand').innerHTML = "";
     dealStartingHand();
-    //let new_button=document.querySelector('.controls');
-    //new_button.firstChild.addEventListener('click',hit())
+    getValueOfCards();
+    let new_button = document.getElementById('stay-btn');
+    new_button.onclick = endTurn;
+    new_button = document.getElementById('hit-btn');
+    new_button.onclick = hit;
 }
-
-/*function round(){
-
-    dealStartingHand();
-    turn='player';
-    while(turn==='player') {
-        document.getElementById('hit-btn').addEventListener('click', showCard('player'))
-        document.getElementById('stay-btn').addEventListener('click', endTurn())
-    }
-    document.getElementById('hit-btn').removeEventListener('click',showCard('player'))
-    document.getElementById('stay-btn').removeEventListener('click',endTurn())
-}*/
 
 function summCardValues(hand) {
     console.log(hand);
-    let summ=1;
-    for(let i of hand){
+    let summ = 1;
+    for (let i of hand) {
         console.log(i + 'this');
-        summ= eval(summ) + eval(i);
+        summ = eval(summ) + eval(i);
     }
     return summ;
 }
 
-
+function getValueOfCards() {
+    playerCardValues = addValuesOfCards(playerCards);
+    bankCardValues = addValuesOfCards(bankCards);
+    intOfPlayerCards = getIntFromObject(playerCardValues);
+    intOfBankCards = getIntFromObject(bankCardValues);
+}
 
 startTurn();
-playerCardValues = addValuesOfCards(playerCards);
-bankCardValues = addValuesOfCards(bankCards);
-intOfPlayerCards = getIntFromObject(playerCardValues);
-intOfBankCards = getIntFromObject(bankCardValues);
-console.log(intOfPlayerCards);
+
 
